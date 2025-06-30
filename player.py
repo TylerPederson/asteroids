e2dc80cb91d2f08951640413df51de4967cca394
tylerpederson <tyler.lee.pederson@gmail.com>
2025-06-30 11:06:37 -0700

    Add shooting mechanic

--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame
 from circleshape import *
 from constants import *
+from shot import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -26,8 +28,17 @@ class Player(CircleShape):
         rot_vec = up_vec.rotate(self.rotation)
         rot_vec *= PLAYER_SPEED * scale * dt
         self.position += rot_vec
+
+    def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
     
     def update(self, dt):
+        self.shoot_timer -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -38,3 +49,5 @@ class Player(CircleShape):
             self.move(1, dt)
         if keys[pygame.K_s]:
             self.move(-1, dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
